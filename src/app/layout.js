8a4d67674b232b94ae597a8e78a8b162ad0e2e35
Varad <varaddhabe0@gmail.com>
2026-05-15 21:47:43 +0530

    removed lenis smooth scroll

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SmoothScroll from "@/components/SmoothScroll";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +24,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body>
-        <SmoothScroll>
+        
           {children}
-        </SmoothScroll>
+        
       </body>
     </html>
   );
--- src/context/LenisContext.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext, useContext } from "react";
-
-export const LenisContext = createContext(null);
-
-export function useLenis() {
-  return useContext(LenisContext);
-}
